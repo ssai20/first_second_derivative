b4dec114c421229e2b867e71c36ce374fd9481d9
Shagaev <ssairan20@gmail.com>
2023-08-01 21:25:36 +0600

    First task

--- Shagaev_Java_1.java
+++ Shagaev_Java_1.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class HiTask {
+public class Shagaev_Java_1 {
     public static void main(String[] args) throws IOException {
         try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
             int s = Integer.parseInt(br.readLine());
